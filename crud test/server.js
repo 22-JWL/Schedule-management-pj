6ed72c88e29a1a9e7b6c0f274f874a232dce8b64
22-JWL <100843910+22-JWL@users.noreply.github.com>
2024-02-09 19:26:09 +0900

    feat: add body-parser

--- crud test/server.js
+++ crud test/server.js
@@ -7,6 +7,9 @@ const port = 8080;
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+//form body parser
+app.use(express.urlencoded({ extended: false }));
+
 app.listen(port, function () {
   console.log(`Server is running on ${port}`);
 });
@@ -17,9 +20,21 @@ app.get("/", function (req, res) {
 });
 
 app.get("/booking", function (req, res) {
-  res.send("booking page");
+  //   res.send("booking page");
+  res.render("booking");
 });
 
 app.get("/statistics", function (req, res) {
   res.send("statistics page");
+});
+
+app.post("/checkResult", (req, res) => {
+  console.log(req.body);
+  console.log(req.body);
+
+  let result = req.body;
+
+  res.send(
+    `<h1>${result.name}님은 ${result.attendance}로 등록되었습니다.</h1>`
+  );
 });
